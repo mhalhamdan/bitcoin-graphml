55e122ad262cf74fbdcad0697246ff6e8f5a0915
mhalhamdan <muhammad.alhamdan@gmail.com>
2021-11-15 22:12:14 -0500

    Added correlation matrix

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from preprocessing import holdout
+from matplotlib.pyplot import axis
+from sklearn import model_selection
+from sklearn import neighbors
+from preprocessing import get_high_correlation, holdout, find_corr, get_high_correlation
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import GridSearchCV
@@ -67,21 +70,21 @@ def train(xTrain, yTrain, model_name="knn", grid_search=False):
                 KNeighborsClassifier(), 
                 [{'n_neighbors': range(1,10,2), 'metric': ['euclidean','manhattan']}], cv=5, scoring='f1_macro' , verbose=1)
         else:
-            model = KNeighborsClassifier()
+            model = KNeighborsClassifier(metric="manhattan")
 
         model.fit(xTrain, yTrain['class'])
         
     # Decision tree
     elif model_name == "dt":
         if grid_search:
-            parameters = {"max_depth":[1,3,5,7,9,11], 'min_samples_leaf':[50,100,150,200,250,300], "criterion":['gini', 'entropy']}
+            parameters = {"max_depth":[1,3,5,7,9,11], 'min_samples_leaf':[2,10,50,100,150,200,250,300], "criterion":['gini', 'entropy']}
             model = GridSearchCV(
                 DecisionTreeClassifier(), 
                 parameters
             )
-            print("DecisionTree best params: ", model.best_params_)
         else:
-            model = DecisionTreeClassifier()
+            # {'criterion': 'entropy', 'max_depth': 11, 'min_samples_leaf': 50}
+            model = DecisionTreeClassifier(criterion='entropy', max_depth=11, min_samples_leaf=10)
 
         model.fit(xTrain, yTrain['class'])
 
@@ -101,11 +104,31 @@ def main():
     # Read data
     y = pd.read_csv("filtered_classes.csv")
     xFeat = pd.read_csv("filtered_features.csv")
+
+    # Find correlation matrix
+    feature_matrix, label_matrix = find_corr(xFeat, y, plot_title=False)
+    
+    # Threshold for cutting out a correlation 
+    to_remove = get_high_correlation(feature_matrix,THRESHOLD=0.95)
+
+    # Drop based on correlation
+    xFeat.drop(to_remove, axis=1, inplace=True)
+
+    # Get correlation after dropping
+    _, _ = find_corr(xFeat, y.drop(["txId"], axis=1), plot_title="Correlation Matrix for Bitcoin dataset")
+
     # Split data, train = 70%, test 30%
     xTrain, xTest, yTrain, yTest = holdout(xFeat, y, 0.7)
 
+    model_name = "knn"
+
     # Initialize and train model
-    model = train(xTrain, yTrain, "knn", grid_search=False)
+    model = train(xTrain, yTrain, model_name, grid_search=False)
+
+    try:
+        print(f"{model_name} best params: {model.best_params_}")
+    except:
+        pass
 
     results = predict(model, xTrain, yTrain, xTest, yTest)
 
